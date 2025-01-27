ec50d972cb6fa8651d4b5365a51656db4be228e7
Arsenyuk <illya.arsenyuk@kpk-lp.com.ua>
2025-01-27 14:21:57 +0200

    Add Vuetify

--- fishin-store-app/src/components/Footer.vue
+++ fishin-store-app/src/components/Footer.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="hello">
+  <div class="footer">
     <h1>{{ msg }}</h1>
-   
+  
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'FooterSite',
   props: {
     msg: String
   }
