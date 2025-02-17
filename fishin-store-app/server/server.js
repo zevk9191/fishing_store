e4e67944426684058be36619db5cac69bfba5e8c
Zakhar <zevk9191@gmail.com>
2025-02-17 20:43:39 +0200

    add server

--- fishin-store-app/server/server.js
+++ fishin-store-app/server/server.js
@@ -1,39 +1,43 @@
-// server.js
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
 
 const app = express();
-app.use(cors()); // Для дозволу запитів з фронтенду
+app.use(cors());
 
 // Підключення до MySQL
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',      // Ваш MySQL користувач
-  password: '1234',      // Ваш MySQL пароль
-  database: 'pubalka' // Ваша база даних
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "fishing_store",
 });
 
 db.connect((err) => {
-  if (err) throw err;
-  console.log('Connected to the MySQL server.');
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to the MySQL server.");
 });
 
-// Створення маршруту для отримання всіх продуктів
-app.get('/api/products', (req, res) => {
-  // Запит до бази даних для отримання всіх продуктів
-  db.query('SELECT * FROM Products', (err, results) => {  // замінено connection на db
+// Маршрут для отримання перших 10 товарів
+app.get("/api/products/first10", (req, res) => {
+  db.query("SELECT * FROM Products LIMIT 10", (err, results) => {
     if (err) {
-      console.error('Error executing query:', err.stack);
-      return res.status(500).send('Database query error');
+      console.error("Error executing query:", err.stack);
+      return res.status(500).send("Database query error");
     }
-
-    // Якщо запит успішний, повертаємо дані у форматі JSON
     res.json(results);
   });
 });
 
+// Головна сторінка
+// app.get("/", (req, res) => {
+//   res.send("Сервер працює!");
+// });
+
 // Запуск сервера
 app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+  console.log("Server is running on http://localhost:3000");
 });
