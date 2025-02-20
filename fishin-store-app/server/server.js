7cc9ad748604f23fbfba59aebcff26c05642d616
Zakhar <zevk9191@gmail.com>
2025-02-20 18:08:53 +0200

    minor fix

--- fishin-store-app/server/server.js
+++ fishin-store-app/server/server.js
@@ -5,7 +5,6 @@ const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const multer = require("multer");
 const path = require("path");
 
 const app = express();
